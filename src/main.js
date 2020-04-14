654369cad858cab0109a72b3b7b5e182aa7d7d07
kind <kind790@163.com>
2020-04-14 19:45:59 +0800

    fixed release path

--- src/main.js
+++ src/main.js
@@ -15,6 +15,9 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 
+// 解决IE浏览器ES6的问题
+// import 'babel-polyfill'
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
